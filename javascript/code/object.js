c010205ba07d7254942506518c3d292d1e950d5e
aghb123 <wangzhuang137@gmail.com>
2021-01-30 22:23:44 +0800

    update

--- javascript/code/object.js
+++ javascript/code/object.js
@@ -1,2 +1,7 @@
-console.log(Math.PI);
-console.log(Math.max());
+/* console.log(Math.PI);
+console.log(Math.max()); */
+var date = new Date();
+console.log(date);
+var date1 = new Date(2019, 10, 1);
+console.log(date1);
+console.log(date.getFullYear());
